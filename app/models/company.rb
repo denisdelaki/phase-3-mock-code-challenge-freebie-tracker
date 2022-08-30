352f7af35e390c8f04547d47c1b58d7263dd2dc2
denisdelaki <denisdelaki@gmail.com>
2022-08-30 13:57:04 +0300

    Done.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,12 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+def give_freebie(dev, item_name, value)
+    #createa new Freebie instance
+    Freebie.create(item_name:item_name, value:value, dev:dev, company:self)
 
+end
+def self.oldest_company
+self.all.order(founding_year: :ASC).first
+end
 end
